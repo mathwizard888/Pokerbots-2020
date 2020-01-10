0a0be8e4232bb04e9806c03e9a7bcd966491e34a
Kevin Liu <kevliu888@dhcp-18-20-140-179.dyn.mit.edu>
2020-01-10 16:20:32 -0500

    particle filter

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -7,12 +7,26 @@ from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
 from skeleton.bot import Bot
 from skeleton.runner import parse_args, run_bot
 import random
+import eval7
+from numpy.random import geometric
 
 
 class Player(Bot):
     '''
     A pokerbot.
     '''
+
+    def permute_values(self):
+        '''
+        Selects a value permutation for the whole game according the prior distribution.
+        '''
+        orig_perm = list(range(13))[::-1]
+        prop_perm = []
+        seed = geometric(p=0.25, size=13) - 1
+        for s in seed:
+            pop_i = len(orig_perm) - 1 - (s % len(orig_perm))
+            prop_perm.append(orig_perm.pop(pop_i))
+        return prop_perm
 
     def __init__(self):
         '''
@@ -27,7 +41,23 @@ class Player(Bot):
         self.VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
         self.wins_dict = {v : 1 for v in self.VALUES}
         self.showdowns_dict = {v : 2 for v in self.VALUES}
-        pass
+        # particle filter
+        values = list('23456789TJQKA')
+        suits = list('cdhs')
+        self.proposal_perms = []
+        for j in range(1000):
+            # proposal_perm is a list with entries from 0 to 12
+            proposal_perm = self.permute_values()
+            perm_dict = {}
+            for i, v in enumerate(values):
+                for s in suits:
+                    card = v + s
+                    permuted_i = proposal_perm[i]
+                    permuted_v = values[permuted_i]
+                    permuted_card = eval7.Card(permuted_v + s)
+                    perm_dict[card] = permuted_card
+            # we've gone through the whole deck
+            self.proposal_perms.append(perm_dict)
 
     def handle_new_round(self, game_state, round_state, active):
         '''
@@ -65,6 +95,7 @@ class Player(Bot):
         street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
         my_cards = previous_state.hands[active]  # your cards
         opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        board_cards = previous_state.deck[:street]
         if opp_cards != []:  # we have a showdown
             if my_delta > 0:  # we won
                 self.wins_dict[my_cards[0][0]] += 1
@@ -76,7 +107,27 @@ class Player(Bot):
                 self.wins_dict[opp_cards[1][0]] += 1
             self.showdowns_dict[opp_cards[0][0]] += 1
             self.showdowns_dict[opp_cards[1][0]] += 1
-        pass
+            # update particle filter
+            new_perms = []
+            for proposal_perm in self.proposal_perms:  # check if valid
+                my_perm_cards = [proposal_perm[c] for c in my_cards]
+                opp_perm_cards = [proposal_perm[c] for c in opp_cards]
+                board_perm_cards = [proposal_perm[c] for c in board_cards]
+                my_cards_available = my_perm_cards + board_perm_cards
+                opp_cards_available = opp_perm_cards + board_perm_cards
+                my_strength = eval7.evaluate(my_cards_available)
+                opp_strength = eval7.evaluate(opp_cards_available)
+                # consistent with my win
+                if my_strength > opp_strength and my_delta > 0:
+                   new_perms.append(proposal_perm)
+                # consistent with opp win
+                if my_strength < opp_strength and my_delta < 0:
+                   new_perms.append(proposal_perm)
+                # consistent with a tie
+                if my_strength == opp_strength and my_delta == 0:
+                   new_perms.append(proposal_perm)
+            if len(new_perms) >= 10:
+                self.proposal_perms = new_perms
 
     def get_action(self, game_state, round_state, active):
         '''

60e3b8a9dfacd5e6a6cf407b115ef2cb22894774
Kevin Liu <kevliu888@dhcp-18-20-246-240.dyn.mit.edu>
2020-01-09 19:49:25 -0500

    first

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -90,24 +90,70 @@ class Player(Bot):
         Returns:
         Your action.
         '''
-        legal_actions = round_state.legal_actions()  # the actions you are allowed to take
-        #street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
-        #my_cards = round_state.hands[active]  # your cards
-        #board_cards = round_state.deck[:street]  # the board cards
-        #my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
-        #opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
-        #my_stack = round_state.stacks[active]  # the number of chips you have remaining
-        #opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
-        #continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
-        #my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
-        #opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
-        #if RaiseAction in legal_actions:
-        #    min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
-        #    min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
-        #    max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
-        if CheckAction in legal_actions:  # check-call
+        if legal_actions == {CheckAction}:
             return CheckAction()
-        return CallAction()
+        street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
+        my_cards = round_state.hands[active]  # your cards
+        board_cards = round_state.deck[:street]  # the board cards
+        my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
+        opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
+        my_stack = round_state.stacks[active]  # the number of chips you have remaining
+        opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
+        continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
+        my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
+        opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+        pot_after_continue = my_contribution + opp_contribution + continue_cost
+        pot_odds = continue_cost / pot_after_continue
+        if RaiseAction in legal_actions:
+            min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+            min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+            first_card_winrate = self.wins_dict[my_cards[0][0]] / self.showdowns_dict[my_cards[0][0]]
+            second_card_winrate = self.wins_dict[my_cards[1][0]] / self.showdowns_dict[my_cards[1][0]]
+            # for indexing
+            winrates = [first_card_winrate, second_card_winrate]
+            # count up how often our cards agree with the board cards
+            agree_counts = [0, 0]
+            for card in board_cards:
+                # increase agree_counts each time values agree
+                for i in range(2):
+                    if my_cards[i][0] == card[0]:
+                        agree_counts[i] += 1
+            raise_amount = my_pip + continue_cost + int(0.75 * pot_after_continue)
+            raise_amount = min(raise_amount, max_raise)
+            raise_amount = max(raise_amount, min_raise)
+            # TWO-PAIR OR BETTER
+            if sum(agree_counts) >= 2:
+                return RaiseAction(raise_amount)
+            # ONE PAIR IN FIRST CARD
+            if agree_counts[0] == 1:
+                if random.random() < winrates[0]:
+                    return RaiseAction(raise_amount)
+                if CheckAction in legal_actions:
+                    return CheckAction()
+                if pot_odds < winrates[0]:
+                    return CallAction()
+            # ONE PAIR IN SECOND CARD
+            if agree_counts[1] == 1:
+                if random.random() < winrates[1]:
+                    return RaiseAction(raise_amount)
+                if CheckAction in legal_actions:
+                    return CheckAction()
+                if pot_odds < winrates[1]:
+                    return CallAction()
+            # POCKET PAIR
+            if my_cards[0][0] == my_cards[1][0]:
+                if random.random() < winrates[0]:
+                    return RaiseAction(raise_amount)
+                if CheckAction in legal_actions:
+                    return CheckAction()
+                if pot_odds < winrates[0]:
+                    return CallAction()
+            if first_card_winrate > 0.5 and second_card_winrate > 0.5 and street < 5:
+                return RaiseAction(min_raise)
+        if CheckAction in legal_actions:
+            return CheckAction()
+        return FoldAction()
 
 
 if __name__ == '__main__':

28a21cdc9a6a5cb6d1c2d9a3270340c30d94faf8
Kevin Liu <kevliu888@dhcp-18-20-246-240.dyn.mit.edu>
2020-01-23 04:45:27 -0500

    eval7 based strength function

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -38,11 +38,12 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        self.VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']        # particle filter
+        self.guar_win = False
+        # particle filter
         values = list('23456789TJQKA')
         suits = list('cdhs')
         self.proposal_perms = []
-        for j in range(50000):
+        for j in range(100000):
             # proposal_perm is a list with entries from 0 to 12
             proposal_perm = self.permute_values()
             perm_dict = {}
@@ -68,12 +69,17 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        #my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
-        #game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
-        #round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
         #my_cards = round_state.hands[active]  # your cards
-        #big_blind = bool(active)  # True if you are the big blind
-        pass
+        big_blind = bool(active)  # True if you are the big blind
+        if game_clock < 1:
+            print(round_num)
+        if round_num == 1000:
+            print(game_clock)
+        if my_bankroll > (1001-round_num) + (1001-round_num-int(big_blind))//2:
+            self.guar_win = True
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
@@ -116,6 +122,48 @@ class Player(Bot):
             if len(new_perms) >= 10:
                 self.proposal_perms = new_perms
 
+    def convert_eval(self, raw_eval):
+        '''
+        empty: 0
+        worst high card: 65536
+        best high card: 834199 (diff 768663)
+        worst pair: 16777216
+        best pair: 17611408 (diff 834192)
+        worst two pair: 33619968
+        best two pair: 34388480 (diff 768512)
+        worst 3 of a kind: 50331648
+        best 3 of a kind: 51165696 (diff 834048)
+        worst straight: 67305472
+        best straight: 67895296 (diff 589824)
+        worst flush: 84226576
+        best flush: 84720279 (diff 493703)
+        worst full house: 100667392
+        best full house: 101494784 (diff 827392)
+        worst 4 of a kind: 117440512
+        best 4 of a kind: 118226944 (diff 786432)
+        worst straight flush: 134414336
+        best straight flush: 135004160 (diff 589824)
+        '''
+        if raw_eval == 0:
+            return 0
+        if raw_eval <= 834199: # high card
+            return (raw_eval-65536)/768663*0.174
+        if raw_eval <= 17611408: # pair
+            return 0.175+(raw_eval-16777216)/834192*0.437
+        if raw_eval <= 34388480: # two pair
+            return 0.613+(raw_eval-33619968)/768512*0.234
+        if raw_eval <= 51165696: # 3 of a kind
+            return 0.848+(raw_eval-50331648)/834048*0.047
+        if raw_eval <= 67895296: # straight
+            return 0.896+(raw_eval-67305472)/589824*0.045
+        if raw_eval <= 84720279: # flush
+            return 0.942+(raw_eval-84226576)/493703*0.029
+        if raw_eval <= 101494784: # full house
+            return 0.972+(raw_eval-100667392)/827392*0.025
+        if raw_eval <= 118226944: # 4 of a kind
+            return 0.998+(raw_eval-117440512)/786432*0.0016
+        return 0.9997+(raw_eval-134414336)/589824*0.0003 # straight/royal flush
+
     def get_action(self, game_state, round_state, active):
         '''
         Where the magic happens - your code should implement this function.
@@ -132,6 +180,10 @@ class Player(Bot):
         legal_actions = round_state.legal_actions()  # the actions you are allowed to take
         if legal_actions == {CheckAction}:
             return CheckAction()
+        if self.guar_win:
+            if CheckAction in legal_actions:
+                return CheckAction()
+            return FoldAction()
         street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
         my_cards = round_state.hands[active]  # your cards
         board_cards = round_state.deck[:street]  # the board cards
@@ -145,87 +197,56 @@ class Player(Bot):
         pot_after_continue = my_contribution + opp_contribution + continue_cost
         pot_odds = continue_cost / pot_after_continue
 
-        # estimate card ranks
+        # find average hand strength
         valid_perms = 0
-        my_ranks = [0,0]
-        my_rel_ranks = [0,0]
-        board_ranks = [0 for _ in range(street)]
+        my_strength = 0
+        board_strength = 0
         for perm in self.proposal_perms:
             valid_perms += 1
+            eval7_pocket = [perm[card] for card in my_cards]
+            eval7_board = [perm[card] for card in board_cards]
+            my_strength += self.convert_eval(eval7.evaluate(eval7_pocket+eval7_board))
+            board_strength += self.convert_eval(eval7.evaluate(eval7_board))
+
+        my_strength /= valid_perms
+        board_strength /= valid_perms
+
+        # potential pair adjustment
+        for card in board_cards:
+            board_strength = 0.8*board_strength+0.2*(0.4+board_strength*0.6)
+
+        # potential flush adjustment
+        my_probs = [[0,0,0.1],[],[],[0,0,0,0.05,0.4,1],[0,0,0,0,0.2,1,1],[0,0,0,0,0,1,1,1]]
+        opp_probs = [[0.05,0.3],[0,0.15,0.6],[0,0.05,0.2,1]]
+        for suit in 'cdhs':
+            my_count = 0
+            board_count = 0
             for i in range(2):
-                my_ranks[i] += perm[my_cards[i]].rank
-                for j in range(street):
-                    if i == 0:
-                        board_ranks[j] += perm[board_cards[j]].rank
-                    if perm[board_cards[j]].rank <= perm[my_cards[i]].rank:
-                        my_rel_ranks[i] += 1
-        for i in range(2):
-            my_ranks[i] /= valid_perms
-            my_rel_ranks[i] /= valid_perms
-        for j in range(street):
-            board_ranks[j] /= valid_perms
-
-        strength = 0  # estimate hand strength
-        
-        # count up how often our cards agree with the board cards
-        agree_counts = [0, 0]
-        board_agree_counts = [0 for _ in range(street)]
-        for j in range(street):
-            # increase agree_counts each time values agree
-            for i in range(2):
-                if my_cards[i][0] == board_cards[j][0]:
-                    agree_counts[i] += 1
-                    board_agree_counts[j] += 1
-                    strength += (my_rel_ranks[i]+street)/(4*street) + agree_counts[i]/20
-
-        board_matches = 0
-        for j in range(street):
-            for i in range(j):
-                if board_cards[i][0] == board_cards[j][0] and board_agree_counts[i] == 0:
-                    board_matches += 1
-                    
-        # pocket pair adjustment
-        if my_cards[0][0] == my_cards[1][0]:
-            if street > 0:
-                strength += (my_rel_ranks[0]+street)/(4*street) + agree_counts[0]/5
-            else:
-                strength += (my_ranks[0]+12)/24
-            if board_matches < 3:
-                strength -= 0.1*board_matches
-        else:
-            strength -= 0.1*board_matches
-            strength += sum(my_ranks)/(30*(street+2))
-
-        # flush adjustment
-        if sum(agree_counts) < 3:
-            my_probs = [[0,0,0.1],[],[],[0,0,0,0.05,0.4,1],[0,0,0,0,0.2,1,1],[0,0,0,0,0,1,1,1]]
-            opp_probs = [[0.05,0.3],[0,0.15,0.6],[0,0.05,0.2,1]]
-            for suit in 'cdhs':
-                my_count = 0
-                board_count = 0
-                for i in range(2):
-                    if my_cards[i][1] == suit:
-                        my_count += 1
-                for i in range(street):
-                    if board_cards[i][1] == suit:
-                        board_count += 1
-                # add for my flush
-                strength += my_probs[street][my_count+board_count]
-                # subtract for opp flush
-                if street > 0 and board_count >= 2:
-                    strength -= (pot_odds+opp_probs[street-3][board_count-2]) / (pot_odds+1)
+                if my_cards[i][1] == suit:
+                    my_count += 1
+            for i in range(street):
+                if board_cards[i][1] == suit:
+                    board_count += 1
+            # add for my flush
+            if my_strength < 0.942:
+                my_strength += my_probs[street][my_count+board_count]*(0.942-my_strength)
+            # subtract for opp flush
+            if street > 0 and board_count >= 2 and board_strength < 0.942:
+                board_strength += opp_probs[street-3][board_count-2]*(0.942-board_strength)
 
         # play based on strength
+        strength = my_strength - board_strength
+        if street == 0:
+            strength /= 0.612
         if pot_odds < strength:
-            if random.random() < strength:
+            if random.random() < strength and RaiseAction in legal_actions:
                 min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
                 min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
                 max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
                 raise_amount = my_pip + continue_cost + int(0.75 * pot_after_continue)
                 raise_amount = min(raise_amount, max_raise)
                 raise_amount = max(raise_amount, min_raise)
-                if RaiseAction in legal_actions:
-                    return RaiseAction(raise_amount)
+                return RaiseAction(raise_amount)
             if CallAction in legal_actions:
                 return CallAction()
             
